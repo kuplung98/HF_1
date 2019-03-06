70f26a2f9b899c0f3230738ff99632052feb8052
kuplung98 <48050589+kuplung98@users.noreply.github.com>
2019-03-06 08:27:27 +0100

    Create main.cpp

---
+++ HF_2/main.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include<cmath>
+
+//template<typename F, typename T>
+
+double sq(double x)
+{
+    return x*x;
+}
+
+template<typename F,typename G, typename T>
+//Find thesquarerootof'num'bystarting the iteration from 'x0'
+T sqrt_newton(F f,G g, T x0)
+{
+  if(x0 <= 0.0)
+  {
+      std::cout<<"No solution. x0 must be positive.\n";
+      return 0.0;
+  }  
+  else{
+  int i = 0;
+  double x1 = 0.0;
+
+
+  while(i <= 10){
+    x1 = x0-f(x0)/g(x0) ;
+    if (abs(x1-x0) <= 1e-12)
+    {    
+        return x1;
+    }
+    else
+    {
+        x0 = x1;
+        i++;
+    }
+  }
+  return x1;
+  }
+}
+
+
+int main(int, char**) {
+   std::cout.precision(16);
+   double s0 = 24.738633753705963298;
+   std::cout<<"The squareroot of 612 is: "<<s0<< "\n\n";
+   double g = sqrt_newton([](double x){ return x*x-612.0; },[](double x){ return 2.0*x; }, 10.0);
+   std::cout<<"The solution is: "<< g <<"\n";
+   std::cout<<"Difference from the test value.:"<< s0-g << "\n";
+}
