ea284466599e93e2e7f26eef4e2fe310be5cf0e3
kuplung98 <48050589+kuplung98@users.noreply.github.com>
2019-05-22 23:37:35 +0200

    Update main.cpp

--- HF_8/main.cpp
+++ HF_8/main.cpp
@@ -11,14 +11,14 @@ int main(int, char**) {
    std::normal_distribution<double> distr(100,20);
    std::ofstream myfile;
    myfile.open("time1.txt",std::fstream::app);
-   for(int N=100;N<=200;N+=100)
+   for(int N=100;N<=1500;N+=100)
    {
-      double min1=1e10;
+      //double min1=1e10;
       double min2=1e10;
       for(int i=0; i<10; i++)
       {
-            Matrix<double> m0(Idx1{},[&](auto i){ return distr(gen); },N);
-            Matrix<double> m1(Idx1{},[&](auto i){ return distr(gen); },N);
+           // Matrix<double> m0(Idx1{},[&](auto i){ return distr(gen); },N);
+           // Matrix<double> m1(Idx1{},[&](auto i){ return distr(gen); },N);
             Matrix<double> m2(Idx1{},[&](auto i){ return distr(gen); },N);
             Matrix<double> m3(Idx1{},[&](auto i){ return distr(gen); },N);
 
@@ -26,10 +26,10 @@ int main(int, char**) {
             parallel(m2,m3);
             auto t3 = std::chrono::high_resolution_clock::now();
 
-           /* min1=std::min(min1,(static_cast<std::chrono::duration<double,std::milli>>(t2-t1)).count());*/
+           // min1=std::min(min1,(static_cast<std::chrono::duration<double,std::milli>>(t2-t1)).count());
             min2=std::min(min2,(static_cast<std::chrono::duration<double,std::milli>>(t3-t2)).count());
       }
-      myfile<<N<<min2<<"\n";
+      myfile<<N<<" "<<min2<<"\n";
       std::cout<<N<<"\n";
    }
    myfile.close();
